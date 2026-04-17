cc19eb4fcf982564ac03d3788303e66d33274b4b
Luis Miguel Duran <luismi13dd@gmail.com>
2026-04-17 21:00:05 +0200

    refactor: remove logs

--- src/features/farms/api/getFarms.ts
+++ src/features/farms/api/getFarms.ts
@@ -1,11 +1,7 @@
 import { useAuthStore } from '../../auth/store/auth.store'
 import { Farm } from './../types/farms.types'
 
-let calls = 0
-
 export const getFarms = async (): Promise<Farm[]> => {
-  calls += 1
-  console.log('GET /farms called', calls)
   const token = useAuthStore.getState().token
 
   const response = await fetch('https://apicore.spherag.com/System/List', {

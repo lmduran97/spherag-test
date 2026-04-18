3f839b36435dc7879e376fbdae9267ff23e35b9c
Luis Miguel Duran <luismi13dd@gmail.com>
2026-04-18 14:36:56 +0200

    refactor: move function to separate file

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,7 +1,14 @@
+import { Linking } from 'react-native'
+
 export const formatDate = (date: string): string => {
   return new Intl.DateTimeFormat('es-Es', {
     day: '2-digit',
     month: '2-digit',
     year: 'numeric'
   }).format(new Date(date))
+}
+
+export const handleOnPressMap = async (lat: string, long: string) => {
+  const url = `https://www.google.com/maps/search/?api=1&query=${lat},${long}`
+  await Linking.openURL(url)
 }

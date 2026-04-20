a58100660df62561bf056caf275f3915793c1fa0
Luis Miguel Duran <luismi13dd@gmail.com>
2026-04-20 12:33:15 +0200

    refactor: improve test

--- src/features/auth/__tests__/login-screen.test.tsx
+++ src/features/auth/__tests__/login-screen.test.tsx
@@ -1,8 +1,9 @@
-import { describe, expect, it, jest } from '@jest/globals'
-import { render, screen } from '@testing-library/react-native'
+import { beforeEach, describe, expect, it, jest } from '@jest/globals'
+import { fireEvent, render, screen } from '@testing-library/react-native'
 import React from 'react'
 
 import Login from '@/app/(public)/login'
+import { LoginRequest, LoginResponse } from '../types/auth.types'
 
 jest.mock('@expo/vector-icons', () => {
   const React = require('react')
@@ -24,10 +25,11 @@ jest.mock('@expo/vector-icons/AntDesign', () => {
   }
 })
 
+const mockReplace = jest.fn()
 jest.mock('expo-router', () => ({
-  router: {
-    replace: jest.fn()
-  }
+  useRouter: () => ({
+    replace: mockReplace
+  })
 }))
 
 const mockSetToken = jest.fn()
@@ -43,20 +45,94 @@ jest.mock('../store/auth.store', () => ({
   }))
 }))
 
+type MutateOptions = {
+  onSuccess?: (data: LoginResponse) => void | Promise<void>
+  onError?: (error: Error) => void | Promise<void>
+}
+const mockMutate = jest.fn(
+  (_variables: LoginRequest, options?: MutateOptions) => {
+    options?.onSuccess?.({
+      accessToken: {
+        token: 'fake-token',
+        expiration: '2026-04-21T10:18:29Z'
+      },
+      refreshToken: {
+        token: 'refresh-fake-token',
+        expiration: '2026-04-21T10:18:29Z'
+      }
+    })
+  }
+)
 jest.mock('../hooks/useLogin', () => ({
   useLogin: () => ({
-    mutate: jest.fn(),
+    mutate: mockMutate,
     isPending: false,
     error: null
   })
 }))
 
 describe('Login screen', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
   it('renders username input, password input and submit button', () => {
     render(<Login />)
 
     expect(screen.getByTestId('username-input')).toBeTruthy()
     expect(screen.getByTestId('password-input')).toBeTruthy()
     expect(screen.getByTestId('login-button')).toBeTruthy()
+  })
+
+  it('check button is not disabled if both fields have at least one character', () => {
+    render(<Login />)
+
+    const usernameInput = screen.getByTestId('username-input')
+    const passwordInput = screen.getByTestId('password-input')
+    const loginButton = screen.getByTestId('login-button')
+
+    expect(loginButton).toBeDisabled()
+
+    fireEvent.changeText(usernameInput, 'user')
+    fireEvent.changeText(passwordInput, 'password')
+
+    expect(loginButton).toBeEnabled()
+  })
+
+  it('check password is visible if toggle show password is pressed', () => {
+    render(<Login />)
+
+    const passwordInput = screen.getByTestId('password-input')
+    const toggleShowPasswordButton = screen.getByTestId(
+      'toggle-show-password-button'
+    )
+
+    expect(passwordInput.props.secureTextEntry).toBe(true)
+    fireEvent.press(toggleShowPasswordButton)
+    expect(passwordInput.props.secureTextEntry).toBe(false)
+  })
+
+  it('calls mutate with credentials and handle login with navigation', async () => {
+    render(<Login />)
+
+    const usernameInput = screen.getByTestId('username-input')
+    const passwordInput = screen.getByTestId('password-input')
+    const loginButton = screen.getByTestId('login-button')
+
+    fireEvent.changeText(usernameInput, 'user')
+    fireEvent.changeText(passwordInput, 'password')
+
+    fireEvent.press(loginButton)
+
+    expect(mockMutate).toHaveBeenCalledWith(
+      {
+        username: 'user',
+        password: 'password'
+      },
+      expect.objectContaining({
+        onSuccess: expect.any(Function)
+      })
+    )
+
+    expect(mockSetToken).toHaveBeenCalledWith('fake-token')
   })
 })
